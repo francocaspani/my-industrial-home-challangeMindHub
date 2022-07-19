f7dee24e12f0926663d55f54da9ed379cb6927bd
Dolores Astesiano <dolores.astesiano@gmail.com>
2022-07-19 18:18:40 -0300

    dolores

---
+++ frontend/src/components/CarouselRooms.jsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../styles/carouselEnvironments.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function CarouselRooms() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 600,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+         {data.map(cities => {
+          return (
+        <SwiperSlide>Slide 1</SwiperSlide>
+          )})}
+      </Swiper>
+    </>
+  );
+}
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CarouselRooms from "../components/CarouselRooms"
 
 function Index() {
   return (
     <div>
         <img src='https://www.porcelanosa.com/trendbook/app/uploads/2019/03/venis-ferroker_aluminio_ferroker_loft_h.jpg' style={{width: '100%', objectFit: 'cover'}}/>
+    <CarouselRooms/>
     </div>
   )
 }
