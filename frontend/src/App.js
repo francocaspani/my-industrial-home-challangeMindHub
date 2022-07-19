74401b354a1ca28e851588bc493711d4bf04b89e
Tomas Demarco <tomasdemarco44@hotmail.com>
2022-07-19 17:35:30 -0300

    Component Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Card from "./components/Card"
+import "./styles/Card.css"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Card/>
     </div>
   );
 }
