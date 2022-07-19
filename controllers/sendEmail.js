83872ec6bb3b0788b603756b02ed3145635c039f
franco caspani <francocasp@gmail.com>
2022-07-19 16:36:12 -0300

    user email

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -20,7 +20,7 @@ const sendEmail = async (email, uniqueString, firstName) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'francocaspani.dev@gmail.com',
+            user: 'myindustrialh@gmail.com',
             type: 'OAuth2',
             clientId: process.env.GOOGLE_CLIENTID,
             clientSecret: process.env.GOOGLE_CLIENTSECRET,
