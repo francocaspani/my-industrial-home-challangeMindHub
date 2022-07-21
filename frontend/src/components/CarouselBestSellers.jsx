a39bf4647bebc435ccf16ca3dfc5acacbab9eb9a
franco caspani <francocasp@gmail.com>
2022-07-21 14:10:08 -0300

    desde dev

--- frontend/src/components/CarouselBestSellers.jsx
+++ frontend/src/components/CarouselBestSellers.jsx
@@ -1,36 +1,35 @@
-// import React, { useRef, useState } from "react";
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-// import "./styles.css";
 
-// // import required modules
-// import { Autoplay, Pagination, Navigation } from "swiper";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 
-// export default function CarouselBestSellers() {
-//     return (
-//     <div>
-//       <Swiper
-//         spaceBetween={30}
-//         centeredSlides={true}
-//         autoplay={{
-//           delay: 2500,
-//           disableOnInteraction: false,
-//         }}
-//         pagination={{
-//           clickable: true,
-//         }}
-//         navigation={true}
-//         modules={[Autoplay, Pagination, Navigation]}
-//         className="mySwiper"
-//       >
-//         <SwiperSlide>Slide 1</SwiperSlide>
-//       </Swiper>
-//     </div>
-//     );
-// }
+export default function CarouselBestSellers() {
+    return (
+    <div>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+      </Swiper>
+    </div>
+    );
+}
