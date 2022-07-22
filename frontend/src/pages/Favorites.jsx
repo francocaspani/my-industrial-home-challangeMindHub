a7b9820d3921b3a4b490ce73c3b8655926f82545
franco caspani <francocasp@gmail.com>
2022-07-22 18:41:21 -0300

    favourites

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -4,38 +4,37 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export default function Favorites() {
 
-const user = useSelector(store => store.usersReducer.userData)
+  const user = useSelector(store => store.usersReducer.userData)
 
-console.log(user)
+  console.log(user)
+  
 
   return (
     <>
-    <div className='Cards'>
-      <div className="card">
-        {user ? user.favourite.map( product => {
-          return(
-            <div className='contentFavorite'>
-          <img className='imgFavorites' src='https://www.hogarmania.com/archivos/201110/lampara-madera-xl-668x400x80xX.jpg' alt='foto'></img>
-          <div>nombre</div>
-          <div>
-            <div>precio</div>
-            <div>90$</div>
-          </div>
-          <button>Add to basket</button>
-          <button>Remove from favourites</button>
+        <div className="containerFavourite">
+          {user ? user.favourite.map(product => {
+            return (
+              <div className='contentFavorite'>
+                <img className='imgFavorites' src='https://www.hogarmania.com/archivos/201110/lampara-madera-xl-668x400x80xX.jpg' alt='foto'></img>
+                <div>nombre</div>
+                <div>
+                  <div>precio</div>
+                  <div>90$</div>
+                </div>
+                <button>Add to basket</button>
+                <button>Remove from favourites</button>
+
+              </div>
+            )
+          }
+
+          )
+
+            : <div><p>Nothing here</p></div>
+          }
 
         </div>
-          )
-        }
 
-        )
-        
-        : <div><p>Nothing here</p></div>
-      }
-        
-      </div>
-    </div>
-    
     </>
   )
 }
